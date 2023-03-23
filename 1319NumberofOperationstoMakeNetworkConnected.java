8bc335ab6ac3e97f86e4d8a3b21413def75f4550
Priyanka <105877016+priyankashah3107@users.noreply.github.com>
2023-03-23 14:34:53 +0530

    DAY 8

---
+++ 1319NumberofOperationstoMakeNetworkConnected.java
@@ -0,0 +1,46 @@
+/*
+ * 1319. Number of Operations to Make Network Connected
+
+ There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+Example 1:
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+
+Example 2:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+ */
+
+ class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1; 
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        int components = n;
+        for (int[] c : connections) {
+            int p1 = findParent(parent, c[0]);
+            int p2 = findParent(parent, c[1]);
+            if (p1 != p2) {
+                parent[p1] = p2; 
+                components--;
+            }
+        }
+        return components - 1;
+    }
+    private int findParent(int[] parent, int i) {
+        if (i == parent[i]) return i;
+        return parent[i] = findParent(parent, parent[i]); 
+    }
+}
