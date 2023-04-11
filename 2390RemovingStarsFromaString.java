66df1a89d9b9c99224f119153d730ec239b79bd3
Priyanka <105877016+priyankashah3107@users.noreply.github.com>
2023-04-11 20:50:21 +0530

    Day 25

---
+++ 2390RemovingStarsFromaString.java
@@ -0,0 +1,88 @@
+/*
+2390. Removing Stars From a String
+
+You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+ 
+
+Example 1:
+
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
+Example 2:
+
+Input: s = "erase*****"
+Output: ""
+Explanation: The entire string is removed, so we return an empty string.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters and stars *.
+The operation above can be performed on s.
+ */
+
+ 
+// Approach Stack Implementation 
+//  TC: O(n+m)
+//  SC: O(n+m)
+// class Solution {
+//     public String removeStars(String s) {
+//       Stack<Character> stack = new Stack<>();
+//       for (char c : s.toCharArray()) {
+//           if(c== '*') {
+//               stack.pop();
+
+//           }
+//           else {
+//               stack.add(c);
+
+//           }
+//       } 
+//       StringBuilder sb = new StringBuilder();
+//       for (char c : stack) {
+//           sb.append(c);
+//       }
+//       return sb.toString();
+//     }
+// }
+
+
+// Approach String Builder 
+// TC O(N)
+// SC O(N)
+
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+
+      for (char c : s.toCharArray()) {
+          if(c== '*') {
+            sb.setLength(sb.length() -1);
+
+          }
+          else {
+            sb.append(c);
+
+          }
+      } 
+      
+      return sb.toString();
+    }
+}
