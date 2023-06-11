1ad8df848670c7137a55fe97416cd500125884de
Priyanka <105877016+priyankashah3107@users.noreply.github.com>
2023-06-11 20:18:25 +0530

    Day 61. 1146. Snapshot Array

---
+++ 1146SnapshotArray.java
@@ -0,0 +1,56 @@
+/*
+1146. Snapshot Array
+Implement a SnapshotArray that supports the following interface:
+
+SnapshotArray(int length) initializes an array-like data structure with the given length. Initially, each element equals 0.
+void set(index, val) sets the element at the given index to be equal to val.
+int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id
+ 
+
+Example 1:
+
+Input: ["SnapshotArray","set","snap","set","get"]
+[[3],[0,5],[],[0,6],[0,0]]
+Output: [null,null,0,null,5]
+Explanation: 
+SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+snapshotArr.set(0,5);  // Set array[0] = 5
+snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+snapshotArr.set(0,6);
+snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+ */ 
+class SnapshotArray{
+    private List<TreeMap<Integer, Integer>> array;
+    private int snapId;
+
+    public SnapshotArray(int length) {
+        array = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            array.add(new TreeMap<>());
+            array.get(i).put(0, 0);
+        }
+        snapId = 0;
+    }
+
+    public void set(int index, int val) {
+        array.get(index).put(snapId, val);
+    }
+
+    public int snap() {
+        return snapId++;
+    }
+
+    public int get(int index, int snapId) {
+        TreeMap<Integer, Integer> history = array.get(index);
+        return history.floorEntry(snapId).getValue();
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
