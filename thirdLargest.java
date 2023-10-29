e8c3ed589aa447d84d25231a592f3221ce781037
Priyanka <105877016+priyankashah3107@users.noreply.github.com>
2023-10-29 13:48:57 +0530

    Third Largest Element

---
+++ thirdLargest.java
@@ -0,0 +1,55 @@
+/*
+ * Problem link: https://practice.geeksforgeeks.org/problems/third-largest-element/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+ * 
+ * Third largest element
+
+Given an array of distinct elements. Find the third largest element in it.
+
+Suppose you have A[] = {1, 2, 3, 4, 5, 6, 7}, its output will be 5 because it is the 3 largest element in the array A.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {2,4,1,3,5}
+Output: 3
+Example 2:
+
+Input:
+N = 2
+A[] = {10,2}
+Output: -1
+Your Task:
+Complete the function thirdLargest() which takes the array a[] and the size of the array, n, as input parameters and returns the third largest element in the array. It return -1 if there are less than 3 elements in the given array.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ A[i] ≤ 105
+
+
+ */
+
+import java.util.Arrays;
+
+public class thirdLargest {
+   
+class Solution
+{
+   
+	   
+    
+    int thirdLargest(int a[], int n)
+    {
+	    // Your code here
+	  if(n<3) {
+	      return -1;
+	  }
+	  Arrays.sort(a);
+	  return a[n-3];
+    }
+}
+ 
+}
